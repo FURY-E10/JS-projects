2291e0a16f746e6de31c3e8c03190625fcbefea1
FURY-E10 <elayapallavan2001@gmail.com>
2024-11-12 11:28:12 +0530

    todo-list with arrays and loops

--- Practice files/11-Arrays and loops/scripts/11todo.js
+++ Practice files/11-Arrays and loops/scripts/11todo.js
@@ -1,12 +1,30 @@
-const todoList = [];
+const todoList = ['Make dinner for her', 'Do dishes with her'];
+
+renderTodoList();
+
+function renderTodoList() {
+
+      let todoListHTML = '';
+
+      for (let i = 0; i < todoList.length; i++) {
+        const todo = todoList[i];
+        const html = `<p>${todo}</p>`;//generating HTML
+        todoListHTML += html
+      }
+
+      console.log(todoListHTML);
+
+      document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+    }
 
 function addTodo() {
   const inputElement = document.querySelector('.js-input');
-  const Name = inputElement.value
+  const name = inputElement.value;
   
-  todoList.push(Name);
+  todoList.push(name);
   console.log(todoList);
 
   inputElement.value = '';
+  renderTodoList();
 }
 
--- Practice files/scripts/11todo.js
+++ Practice files/scripts/11todo.js
@@ -1,8 +1,20 @@
-const todolist = [];
+const todolist = ['make dinner for her', 'wash dishes'];
+
+let todoListHTML = '';
+
+for (let i = 0; i < todolist.length; i++) {
+  const todo = todolist[i];
+  const html = `<p>${todo}</p>`;
+  todoListHTML += html;
+}
+console.log(todoListHTML);
+
+document.querySelector('.js-todo-list')
+  .innerHTML = todoListHTML;
 
 function addTodo() {
   const inputElement = document.querySelector('.js-input');
-  const name = inputElement.value
-  console.log('name')
+  const name = inputElement.value;
+  console.log(name);
 }
 
